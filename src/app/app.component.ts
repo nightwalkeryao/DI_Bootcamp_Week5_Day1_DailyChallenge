adf376ec1143c2b093e24ac756c1b78a59a0488c
NightWalker Yao <huntr@pm.me>
2023-01-17 21:51:02 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    let res = this.http.get('https://swapi.dev/api/films/1/').pipe(
+      map((r: any) => { return r})
+    )
+    res.subscribe(data => console.log(data))
+  }
 }
